5c69468db245907cd073f013ca33be0c001bd8a9
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2022-08-16 19:15:41 +0530

    TypeCastingChar.java added

---
+++ ApnaCollege/TypeCastingChar.java
@@ -0,0 +1,11 @@
+
+public class TypeCastingChar {
+
+    public static void main(String[] args) {
+
+        // Scanner sc = new Scanner(System.in);
+        char ch = 'a';
+        int b = ch;
+        System.out.println(b);
+    }
+}
