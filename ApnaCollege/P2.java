09da738339132cba78741abeae618484c0b1d443
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2022-08-18 22:09:41 +0530

    break keyword

---
+++ ApnaCollege/P2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class P2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Enter Your Number");
+            int n = sc.nextInt();
+            if (n % 10 == 0)
+                break;
+            System.out.println(n);
+
+        } while (true);
+
+    }
+
+}
---
+++ ApnaCollege/Ternary.java
@@ -0,0 +1,15 @@
+public class Ternary {
+
+    public static void main(String[] args) {
+
+        int a = 12;
+        int b = 11;
+        int c = 13;
+
+        // ternary operator
+        int large = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+        System.out.println(large);
+
+    }
+
+}
