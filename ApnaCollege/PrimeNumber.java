326bd8eb31782c137fd3d230d696d3a20cce0988
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2022-08-24 20:32:43 +0530

    Prime Number

--- ApnaCollege/PrimeNumber.java
+++ ApnaCollege/PrimeNumber.java
@@ -3,10 +3,14 @@ public class PrimeNumber {
     public static void main(String[] args) {
 
         PrimeNumber p = new PrimeNumber();
-        p.prime(12);
+        p.prime(2);
     }
 
     void prime(int n) {
+        if (n == 2) {
+            System.out.println("Prime Number");
+            return;
+        }
         for (int i = 2; i <= n - 1; i++) {
             if (n % i == 0) {
                 System.out.println("Prime Number");
