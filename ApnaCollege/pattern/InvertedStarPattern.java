c9abed1eee2f8eb5a4f77d9e6bb31ba24ed23dac
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2022-08-24 22:52:36 +0530

    differnt patterns

--- ApnaCollege/pattern/InvertedStarPattern.java
+++ ApnaCollege/pattern/InvertedStarPattern.java
@@ -1,16 +1,16 @@
 package pattern;
 
-public class StarPattern {
+public class InvertedStarPattern {
 
     public static void main(String[] args) {
 
-        int n = 5;
+        int n = 10;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
+
+            for (int j = 1; j <= n - i + 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
-
         }
     }
 
