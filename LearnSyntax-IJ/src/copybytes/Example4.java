961324a91d9dd243df6a42d3a3a46c90c2b8aebb
sagar <sagar-iitg>
2022-11-17 20:28:24 +0530

    added basic java syntax

---
+++ LearnSyntax-IJ/src/copybytes/Example4.java
@@ -0,0 +1,20 @@
+package copybytes;
+
+
+
+
+import primitives.*;
+
+import primitives.pkg.Main;
+
+
+public class Example4 {
+
+    public static void main(String[] args) {
+
+        Solution.fun1();
+        Main.fun();
+
+
+    }
+}
---
+++ LearnSyntax-IJ/src/primitives/pkg/Main.java
@@ -0,0 +1,9 @@
+package primitives.pkg;
+
+public class Main {
+
+    public static void fun()
+    {
+        System.out.println("primitives.pkg");
+    }
+}
