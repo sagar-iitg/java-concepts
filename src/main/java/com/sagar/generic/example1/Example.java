f9373d24a3fc1ef5496448baef5dc0bad4651470
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-11-13 19:13:42 +0530

    Update Example.java

--- src/main/java/com/sagar/generic/example1/Example.java
+++ src/main/java/com/sagar/generic/example1/Example.java
@@ -9,8 +9,11 @@ public class Example {
     public static void main(String[] args) {
         List l= new ArrayList();
         l.add("India");
+
+       // System.out.println(l.getClass().getName());
         String s = (String) l.get(0);     // typecasting without generics
         System.out.println(s);
+
 
 
 
