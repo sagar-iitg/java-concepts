e603c5cb0eafcea8412b9eeaca7edb8487208291
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-01-22 15:18:44 +0530

    Create Example3.java

---
+++ src/main/java/com/sagar/arrays/pkg/Example3.java
@@ -0,0 +1,29 @@
+package com.sagar.arrays.pkg;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class Example3 {
+    public static void main(String[] args) {
+        //list of list
+        List<Student> list=new ArrayList<>();
+        list.add(new Student(1,"abc",89000));
+        list.add(new Student(1,"abc",70010));
+        System.out.println(list);
+
+
+        for(int i=0;i<list.size();i++){
+            for(int j=i+1;j<list.size();j++)
+                if(list.get(i).getId()==list.get(j).getId()){
+                   list.remove(i);
+                }
+
+        }
+        System.out.println(list);
+
+
+
+    }
+}
