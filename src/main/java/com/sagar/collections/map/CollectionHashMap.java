a7ea1521c2870783209ad2fadad7b8c4e2228ce3
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-01-24 17:20:19 +0530

    Update CollectionHashMap.java

--- src/main/java/com/sagar/collections/map/CollectionHashMap.java
+++ src/main/java/com/sagar/collections/map/CollectionHashMap.java
@@ -3,7 +3,7 @@ package collections.map;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CollectionHashMap {
+public class 	CollectionHashMap {
 
 	public static void main(String[] args) {
 		Map<String, Integer> map = new HashMap<>();
