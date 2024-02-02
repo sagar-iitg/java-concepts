50da4585c961a57475ba991c54bc23701af36ed4
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-02-02 17:29:33 +0530

    Create SystemClass.java

---
+++ src/main/java/com/sagar/basic/SystemClass.java
@@ -0,0 +1,8 @@
+package com.sagar.basic;
+
+public class SystemClass {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(System.getenv());
+    }
+}
