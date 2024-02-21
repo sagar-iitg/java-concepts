f12d43b01e0a82d618a4db8ac30f8cdffca9413a
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-02-22 00:17:27 +0530

    Create Example.java

---
+++ src/main/java/com/sagar/env/Example.java
@@ -0,0 +1,11 @@
+package com.sagar.env;
+
+public class Example {
+    public static void main(String[] args) {
+
+        int countLogicalProcess=Runtime.getRuntime().availableProcessors();
+        System.out.println(countLogicalProcess);
+
+
+    }
+}
