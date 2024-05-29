4d62bea14b591bae286f92d84e017e2e81e9f343
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-05-29 10:42:02 +0530

    Update Main.java

--- src/main/java/com/sagar/basic/Main.java
+++ src/main/java/com/sagar/basic/Main.java
@@ -9,6 +9,8 @@ public class Main implements A{
     }
 
     public static void main(String[] args) {
+        System.out.println("hello"+2*30);
+        System.out.println(2*30+"hello");
         Main obj=new Main();
         obj.method();
     }
