7314a19784d62458b63c749a78be7f081c3810c5
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-06-05 17:31:14 +0530

    Create FlattenArray.java

---
+++ src/main/java/com/sagar/FlattenArray.java
@@ -0,0 +1,34 @@
+package com.sagar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FlattenArray {
+    public static void main(String[] args) {
+        //[[[1],2],[3,4]] Output -> [1,2,3,4]
+        Object[] array = { 1, 2, new Object[]{ 3, 4, new Object[]{ 5 }, 6, 7 }, 8, 9, 10 };
+        Integer[] result=flatten(array);
+        System.out.println(Arrays.toString(result));
+    }
+    public static Integer[] flatten(Object[] array){
+
+        List<Integer> ans =new ArrayList<>();
+        flattenHelper(array,ans);
+        return ans.toArray(new Integer[0]);
+
+    }
+    public static void flattenHelper(Object[] array, List<Integer> ans){
+
+        for (Object ele:array) {
+            if(ele instanceof Integer){
+                ans.add((Integer) ele);
+            }
+            else if(ele instanceof Object[]){
+                flattenHelper((Object[]) ele,ans);
+            }
+        }
+
+    }
+
+}
