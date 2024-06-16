b72f0b7c38f7adc428284886b8f2c014a0a31191
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-06-16 11:13:05 +0530

    Create Example.java

---
+++ src/main/java/com/sagar/intchar/Example.java
@@ -0,0 +1,11 @@
+package com.sagar.intchar;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        int a='2';
+        int b='@';
+        System.out.println(a+" "+b);
+    }
+}
