4704175e27303da1077c4cdab314803e08919313
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-06-17 21:22:45 +0530

    Create Example2.java

---
+++ src/main/java/com/sagar/math/Example2.java
@@ -0,0 +1,9 @@
+package com.sagar.math;
+
+public class Example2 {
+    public static void main(String[] args) {
+
+        int sqrt = (int)Math.sqrt(2147483600);
+        System.out.println(sqrt);
+    }
+}
