a5b81f61a80747a911fbef96f1c62dcf69a30900
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-08-15 11:11:49 +0530

    Create FailSafeExample.java

---
+++ src/main/java/com/sagar/interview3/sapient/FailSafeExample.java
@@ -0,0 +1,21 @@
+package com.sagar.interview3.sapient;
+
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class FailSafeExample {
+    public static void main(String[] args) {
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+            list.add("D"); // Modifying the list during iteration
+        }
+
+        System.out.println("Final List: " + list);
+    }
+}
