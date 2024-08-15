6e1809f3f45dc9eccd4d927690b7b9d0da0c2382
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-08-15 11:11:45 +0530

    Create FailFastExample.java

---
+++ src/main/java/com/sagar/interview3/sapient/FailFastExample.java
@@ -0,0 +1,20 @@
+package com.sagar.interview3.sapient;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class FailFastExample {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+            list.add("D"); // Modifying the list during iteration
+        }
+    }
+}
