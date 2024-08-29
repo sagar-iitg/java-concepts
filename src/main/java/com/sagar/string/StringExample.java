eaa1dfe64db22ab8221a8bd11768cd62dc814d30
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-08-30 00:18:27 +0530

    adding more concepts like codepoint

--- src/main/java/com/sagar/string/StringExample.java
+++ src/main/java/com/sagar/string/StringExample.java
@@ -6,13 +6,13 @@ public class StringExample {
 
 
     public static void main(String[] args) {
-//        byte[]  b={97,98,};
-//        String str=new String(b, Charset.defaultCharset());
-//        System.out.println(str);
-//        System.out.println(Charset.availableCharsets());
+        byte[]  b={97,98,};
+        String str=new String(b, Charset.defaultCharset());
+        System.out.println(str);
+        System.out.println(Charset.availableCharsets());
         String s="sagar";
-        System.out.println(s.codePointAt(0));
-        System.out.println("bcdd".compareTo("acd"));
+        System.out.println(s.codePointAt(1));
+        System.out.println("abc".compareTo("acd"));
 
 
 
