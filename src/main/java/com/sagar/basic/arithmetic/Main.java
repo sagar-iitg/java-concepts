7e81243ea7fb1a674b62799af6105271537e9aeb
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-10-27 15:28:35 +0530

    Create Main.java

---
+++ src/main/java/com/sagar/basic/arithmetic/Main.java
@@ -0,0 +1,9 @@
+package com.sagar.basic.arithmetic;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(-3%7);
+        System.out.println(-8%7);
+    }
+}
