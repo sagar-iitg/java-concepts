34fcc26a975a9b6a2325407b125e1cdf4f27dc87
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-10-29 09:49:04 +0530

    Update Main.java

--- src/main/java/com/sagar/basic/arithmetic/Main.java
+++ src/main/java/com/sagar/basic/arithmetic/Main.java
@@ -1,9 +1,20 @@
 package com.sagar.basic.arithmetic;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println(-3%7);
         System.out.println(-8%7);
-    }
+        Map<Integer,Integer> map=new TreeMap<>();
+        map.put(1,30);
+        map.put(2,30);
+        map.put(3,30);
+        map.put(4,30);
+        System.out.println(map.get(5));
+     }
 }
